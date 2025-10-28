e923bccba7c8cb7e02a0302d9fa8dcc4e07bd1ec
Sunny <pandeysunny01@gmail.com>
2025-10-28 12:05:38 +0530

    updated css js and other files

--- src/main/java/com/sunny/fileConverter/Controller/RedirectController.java
+++ src/main/java/com/sunny/fileConverter/Controller/RedirectController.java
@@ -7,78 +7,85 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class RedirectController {
 
-	@GetMapping(value = "/searchHtml2Pdf")
-	public ModelAndView searchHtml2Pdf(ModelAndView model) {
-
-		model.setViewName("PDF/html2pdf");
-		return model;
-	}
-	
-	@GetMapping(value = "/searchImageToPdfView")
-	public ModelAndView searchJpgToPdf(ModelAndView model) {
-
-		model.setViewName("PDF/imageToPdf/ImageToPdf");
-		return model;
-	}
-	
-	@GetMapping(value = "/searchPdfToJpgView")
-	public ModelAndView searchPdfToJpgView(ModelAndView model) {
-
-		model.setViewName("PDF/pdfToImage/pdfToJpg");
-		return model;
-	}
-
-	@GetMapping(value = "/getPdfToJpgView")
-	public ModelAndView getCommonPageForConverted(ModelAndView model) {
-
-		model.setViewName("PDF/pdfToImage/pdfToImageView");
-		return model;
-	}
-	
-	@GetMapping(value = "/getJpgToPdfView")
-	public ModelAndView getJpgToPdfView(ModelAndView model) {
-
-		model.setViewName("PDF/imageToPdf/ImageToPdfView");
-		return model;
-	}
-	
-	@GetMapping(value = "/getPdfToTextView")
-	public ModelAndView getPdfToTextView(ModelAndView model) {
-
-		model.setViewName("PDF/pdfToText/pdfToTextView");
-		return model;
-	}
-	
-	@GetMapping(value = "/searchPdfToTextView")
-	public ModelAndView searchPdfToTextView(ModelAndView model) {
-
-		model.setViewName("PDF/pdfToText/pdfToText");
-		return model;
-	}
-	
-	@GetMapping(value = "/getImageToTextView")
-	public ModelAndView getImageToTextView(ModelAndView model) {
-
-		model.setViewName("Image/imageToTextView");
-		return model;
-	}
-	
-	@GetMapping(value = "/searchImageToTextView")
-	public ModelAndView searchImageToTextView(ModelAndView model) {
-
-		model.setViewName("Image/imageToText");
-		return model;
-	}
-	
-	@GetMapping(value = "/searchPdfToDocView")
-	public ModelAndView searchPdfToDocView(ModelAndView model) {
-
-		model.setViewName("PDF/PDFtoDOC/addPdfToDoc");
-		return model;
-	}
+//	@GetMapping(value = "/searchHtml2Pdf")
+//	public ModelAndView searchHtml2Pdf(ModelAndView model) {
+//
+//		model.setViewName("PDF/html2pdf");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/searchImageToPdfView")
+//	public ModelAndView searchJpgToPdf(ModelAndView model) {
+//
+//		model.setViewName("PDF/imageToPdf/ImageToPdf");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/searchPdfToJpgView")
+//	public ModelAndView searchPdfToJpgView(ModelAndView model) {
+//
+//		model.setViewName("PDF/pdfToImage/pdfToJpg");
+//		return model;
+//	}
+//
+//	@GetMapping(value = "/getPdfToJpgView")
+//	public ModelAndView getCommonPageForConverted(ModelAndView model) {
+//
+//		model.setViewName("PDF/pdfToImage/pdfToImageView");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/getJpgToPdfView")
+//	public ModelAndView getJpgToPdfView(ModelAndView model) {
+//
+//		model.setViewName("PDF/imageToPdf/ImageToPdfView");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/getPdfToTextView")
+//	public ModelAndView getPdfToTextView(ModelAndView model) {
+//
+//		model.setViewName("PDF/pdfToText/pdfToTextView");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/searchPdfToTextView")
+//	public ModelAndView searchPdfToTextView(ModelAndView model) {
+//
+//		model.setViewName("PDF/pdfToText/pdfToText");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/getImageToTextView")
+//	public ModelAndView getImageToTextView(ModelAndView model) {
+//
+//		model.setViewName("Image/imageToTextView");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/searchImageToTextView")
+//	public ModelAndView searchImageToTextView(ModelAndView model) {
+//
+//		model.setViewName("Image/imageToText");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/searchPdfToDocView")
+//	public ModelAndView searchPdfToDocView(ModelAndView model) {
+//
+//		model.setViewName("PDF/PDFtoDOC/addPdfToDoc");
+//		return model;
+//	}
+//	
+//	@GetMapping(value = "/searchPdfToDocView")
+//	public ModelAndView searchPdfToDocView(ModelAndView model) {
+//
+//		model.setViewName("PDF/PDFtoDOC/addPdfToDoc");
+//		return model;
+//	}
 	
 	@GetMapping(value = "/tools")
-	public ModelAndView searchImageToTextView(ModelAndView model) {
+	public ModelAndView tools(ModelAndView model) {
 
 		model.setViewName("tools");
 		return model;
@@ -88,13 +95,6 @@ public class RedirectController {
 	public ModelAndView fileEditing(ModelAndView model) {
 
 		model.setViewName("fileEditing");
-		return model;
-	}
-	
-	@GetMapping(value = "/searchPdfToDocView")
-	public ModelAndView searchPdfToDocView(ModelAndView model) {
-
-		model.setViewName("PDF/PDFtoDOC/addPdfToDoc");
 		return model;
 	}
 	
